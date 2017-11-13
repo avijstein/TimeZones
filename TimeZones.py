95eaff43e933cae20005c1da6c55fcbc3ad600a4
avijstein <ajstein@wesleyan.edu>
2017-11-13 13:48:38 -0500

    Wrote functions to calculate markers throughtout the day, combined them.

--- TimeZones.py
+++ TimeZones.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta, date, time
 import requests, json
 # import matplotlib.pyplot as plt
 # import pandas as pd
@@ -15,13 +15,11 @@ def conTimeZones(longitude_shift):
         return('Please enter valid distance.')
 
     now = datetime.utcnow()
-
     shift_sec = longitude_shift*240
     if shift_sec >= 0:
         diff = -timedelta(seconds = abs(shift_sec))
     else:
         diff = timedelta(seconds = abs(shift_sec))
-
     return(diff)
 
 
@@ -31,39 +29,7 @@ nyc = [40.730610, -73.935242]
 philly = [39.952583, -75.165222]
 durham = [35.994034, -78.898621]
 
-# print('durham time: ', now + (conTimeZones(durham[1])))
-# print('indianapolis time: ', now + conTimeZones(indianapolis[1]))
-# print('philly time: ', now + conTimeZones(philly[1]))
-# print('UTC time: ', now)
-# print(now + conTimeZones(78.898621))
-# print('\n')
-# print(conTimeZones(durham[1]))
-# print('\n')
-
-# Sunrise Sunset
-# city = durham
-# payload = {'lat':city[0], 'lng':0.0}
-# r = requests.get('https://api.sunrise-sunset.org/json', params=payload)
-#
-# print(r.json())
-# q = r.json()['results']
-# # qtime = q['sunrise']
-# # print(qtime)
-#
-# day_markers = [q['sunrise'], q['solar_noon'], q['sunset'], q['day_length']]
-# print('old day_markers: ', day_markers)
-#
-# for i in range(0,3):
-#     day_markers[i] = datetime.strptime(day_markers[i], '%I:%M:%S %p')
-#     day_markers[i] = day_markers[i] + conTimeZones(city[1])
-#     day_markers[i] = datetime.time(day_markers[i])
-#
-# # print('new day_markers: ', day_markers)
-# marker_names = ['sunrise','solar_noon','sunset','day_length']
-# for i in range(0,len(day_markers)):
-#     print(marker_names[i], '\t', day_markers[i])
-
-
+# Sunrise Sunset Markers
 def DayMarkers(city):
     """
     This function takes a city's latitude and longitude and calculates
@@ -77,16 +43,37 @@ def DayMarkers(city):
         day_markers[i] = datetime.strptime(day_markers[i], '%I:%M:%S %p')
         day_markers[i] = day_markers[i] + conTimeZones(city[1])
         day_markers[i] = datetime.time(day_markers[i])
-
     return(day_markers)
 
-x = DayMarkers(durham)
+# Work Day Markers
+def WorkMarkers(city):
+    """
+    This function takes XY coordinates and calculates the traditional
+    "9-5" workday for that location in the UTC-0 time.
+    """
+    work_start = datetime.combine(datetime.utcnow(), time(hour = 9))
+    work_lunch = datetime.combine(datetime.utcnow(), time(hour = 12))
+    work_end = datetime.combine(datetime.utcnow(), time(hour = 17))
+    shift = conTimeZones(city[1])
+    work_param = [(work_start+shift),(work_lunch+shift),(work_end+shift)]
+    for i in range(0,3):
+        work_param[i] = datetime.time(work_param[i])
+    return(work_param)
 
-marker_names = ['sunrise','solar_noon','sunset','day_length']
-print('CITY: Durham, NC')
-for i in range(0,len(x)):
-    print(marker_names[i], '\t', x[i])
+day = DayMarkers(durham)
+work = WorkMarkers(durham)
 
+marker_names = ['sunrise','solar_noon','sunset','day_length',
+                'work_start','work_lunch','work_end']
+
+
+all_markers = day + work
+
+for i in range(0,len(all_markers)):
+    if marker_names[i] == 'sunset':
+        print(marker_names[i], '\t', '\t', all_markers[i])
+    else:
+        print(marker_names[i], '\t', all_markers[i])
 
 
 
