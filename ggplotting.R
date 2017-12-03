f1fb90b6a3117ca3368a51827a3001cc55bd587b
avijstein <ajstein@wesleyan.edu>
2017-12-03 14:02:44 -0500

    Wrote a wrap-around function to workaround the out-of-bounds problem.

--- ggplotting.R
+++ ggplotting.R
@@ -1,30 +1,32 @@
 setwd('~/Desktop/Real Life/Coding Projects/TimeZones/')
 library(tidyverse)
 
-wrap = data.frame(start = c(5, 18, 18, 0),
-                  end = c(10, 6, 24, 6),
+wrap = data.frame(start = c(5, 18, 9, 12),
+                  end = c(10, 6, 13, 16),
                   band = 1:4)
 
-wrap2 = data.frame(daytime = c(8, 2, 14, 22, 4)) %>%
-  mutate(nighttime = 24 - daytime,
-         band = 1:5)
 
-# maybe using start and ends of times?
+wrap_about = function(times){
+  # Takes times split over 24/0 hour boundary and makes them into two rows.
+  for (i in 1:nrow(times)){
+    print(i)
+    if(times$start[i] > times$end[i]){
+      new_row1 = c(times$start[i], 24, times$band[i])
+      new_row2 = c(0, times$end[i], times$band[i])
+      times = rbind(times[1:(i-1),], new_row1, new_row2, times[(i+1):nrow(times),])
+    }
+  }
+  return(times)
+}
+
+wrap = wrap_about(wrap)
+
+
 ggplot(data = wrap, aes(x = band, fill = factor(band))) +
-  geom_rect(aes(xmin = 0+band-.4, xmax = 0+band+.4, ymin = start, ymax=end), alpha = .6) +
+  geom_rect(aes(xmin = 0+band-.4, xmax = 0+band+.4, ymin = start, ymax=end), alpha = .75) +
   lims(x = c(0,8), y = c(0,24)) +
   coord_polar(theta = 'y', start = pi) +
+  scale_fill_discrete(name = 'Bands') +
   theme_minimal()
 
-
-# maybe using the lengths of days? this doesn't quite work.
-ggplot(data = wrap2, aes(fill = factor(band))) +
-  geom_rect(aes(xmin=band, xmax=band+.1, ymin=daytime, ymax=nighttime), alpha = .6) +
-  coord_polar(theta = 'y') +
-  lims(x = c(0,8), y = c(0,24)) +
-  theme_minimal()
-
-
-# major issue is not being able to cross over the 24-0 hour boundary. 
-# not sure if I should keep looking for solution or hack a workaround.
 
