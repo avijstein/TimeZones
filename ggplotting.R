6586653af9f3a62dffb9016baf2fc21eb21cc0af
avijstein <ajstein@wesleyan.edu>
2017-12-05 11:26:28 -0500

    Up to speed on (now surpassing) design in ggplot vs matplotlib.

--- ggplotting.R
+++ ggplotting.R
@@ -25,51 +25,27 @@ wrap_about = function(times){
 wrap2 = wrap_about(wrap)
 
 
-# just a test to generate random ranges and plot those.
-breakme = data.frame(start = sample(0:24, 5, replace=T),
-                     end = sample(0:24, 5, replace=T),
-                     band = 1:5)
-breakme2 = wrap_about(breakme)
-
-ggplot(data = breakme2, aes(x = band, fill = factor(band))) +
-  geom_rect(aes(xmin = 0+band-.4, xmax = 0+band+.4, ymin = start, ymax=end), alpha = .75) +
-  lims(x = c(0,8), y = c(0,24)) +
-  coord_polar(theta = 'y', start = pi) +
-  scale_fill_discrete(name = 'Bands') +
-  theme_minimal()
-
-
-
 # working on plotting the rings i'll be actually plotting.
-actual_set = data.frame(start = c(12, 14),
-                        end = c(22, 22),
-                        # color = c('orange', 'green'),
-                        color = c(1,2),
-                        band = c(1,2))
+actual_set = data.frame(start = c(13, 14), end = c(22, 22), color = c(1,2), band = c(4,5))
 
 actual_set = actual_set %>%
   rbind(actual_set %>%
         rename(end = start, start = end) %>%
-        # mutate(color = c('blue', 'red')))
         mutate(color = c(3, 4)))
 
 actual_set2 = wrap_about(actual_set)
+colors = c('#f4c20d', '#3cba54', '#4885ed', '#db3236')
 
-# haven't been able to customize colors yet, but i'm  getting there.
-actual_set2 = actual_set2 %>%
-              # mutate(color = as.factor(color)) %>%
-              mutate(recode(color, 'orange' = 1,
-                                   'green' = 2,
-                                   'blue' = 3,
-                                   'red' = 4))
-
-# color = c('orange', 'green', 'blue', 'red'))
-str(actual_set2)
 
 ggplot(data = actual_set2, aes(x = band, fill = factor(color))) +
-  geom_rect(aes(xmin = band-.4, xmax = band+.4, ymin = start, ymax=end), alpha = .75) +
+  geom_rect(aes(xmin = band-.45, xmax = band+.45, ymin = start, ymax=end), alpha = 1) +
+  geom_rect(aes(xmin = 2, xmax = 6.5, ymin = 11.95, ymax = 12.05), fill = '#808080', alpha = 1) +
+  annotate("text", x = 0, y = 0, label = "City Name", size = 8) +
+  annotate("text", x = 7, y = 12, label = 'Now', size = 4) +
   lims(x = c(0,8), y = c(0,24)) +
   coord_polar(theta = 'y', start = pi) +
-  scale_fill_discrete(name = 'Bands') +
-  theme_minimal()
+  scale_fill_manual(name = 'Time of Day', values = colors, labels = c('daytime', 'office', 'nighttime', 'home')) +
+  theme_void() + theme(legend.title = element_text())
+
+
 
